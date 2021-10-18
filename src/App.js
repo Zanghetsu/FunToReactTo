facf8569169890b7b0dff43a70e25676e1b568a2
Zanghetsu <boythasx@gmail.com>
2021-10-18 18:15:09 +0200

    Refactored code base to use function based components, and useState hook

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import React,{ Component } from 'react';
+import React,{ useState } from 'react';
 import './App.css';
 import Jedi from './Jedi/Jedi';
 
-class App extends Component {
-  state = {
-    jedies: [
-      {name : "Obi-Wan Kenobi", age : 24},
-      {name : "Master Yoda", age : 860},
-      {name : "Mace Windu", age : 37}
-     ]
-  }
 
-  switchJediName = () => {
-    this.setState({
+
+const App = props => {
+
+
+  
+  const [jediState, setJediState] = useState({
       jedies: [
-        {name : "Ben Kenobi", age : 42},
+        {name : "Obi-Wan Kenobi", age : 24},
+        {name : "Master Yoda", age : 860},
+        {name : "Mace Windu", age : 37}
+       ]
+    
+  });
+
+  const switchJediName = () => {
+    setJediState({
+      jedies: [
+        {name : "Ben Kenobi", age : 46},
         {name : "Master Yoda", age : 900},
         {name : "Mace Windu", age : 37}
-       ],
-       otherState: 'some other value'
+       ]  
     })
-  }
+  };
 
-  render(){
-    return (
-      <div>
-        <h1>Hello there!</h1>
-        <button onClick={this.switchJediName}>Get Jedi info during the Empire's time!</button>
-        <Jedi name = {this.state.jedies[0].name} age={this.state.jedies[0].age} />
-        <Jedi name = {this.state.jedies[1].name} age={this.state.jedies[1].age} />
-        <Jedi name = {this.state.jedies[2].name} age={this.state.jedies[2].age} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Hello there!</h1>
+      <button onClick={switchJediName}>Get Jedi info during the Empire's time!</button>
+      <Jedi name = {jediState.jedies[0].name} age={jediState.jedies[0].age} />
+      <Jedi name = {jediState.jedies[1].name} age={jediState.jedies[1].age} />
+      <Jedi name = {jediState.jedies[2].name} age={jediState.jedies[2].age} />
+    </div>
+  );
 }
+
 
 export default App;
