16573b62498319b0cd058cf7f8193ad7b99d43d8
Zanghetsu <boythasx@gmail.com>
2021-10-18 17:03:17 +0200

    Basic way of state usage

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Jedi from './Jedi/Jedi';
 
 class App extends Component {
+  state = {
+    jedies: [
+      {name : "Obi-Wan Kenobi", age : 24},
+      {name : "Master Yoda", age : 860},
+      {name : "Mace Windu", age : 37}
+      
+     ]
+  }
+
+
   render(){
     return (
       <div>
         <h1>Hello there!</h1>
         <button>Get Jedi!</button>
-        <Jedi name="Obi-Wan Kenobi" age="24" />
-        <Jedi name="Master Yoda" age="860">Funny this is...</Jedi>
-        <Jedi name="Mace Windu" age="37" />
+        <Jedi name = {this.state.jedies[0].name} age={this.state.jedies[0].age} />
+        <Jedi name = {this.state.jedies[1].name} age={this.state.jedies[1].age} />
+        <Jedi name = {this.state.jedies[2].name} age={this.state.jedies[2].age} />
       </div>
     );
   }
