b630886705c35e4a729a27d9711245223cab5ec5
Zanghetsu <boythasx@gmail.com>
2021-10-18 20:08:55 +0200

    experimented with a little inline styling and css

--- src/App.js
+++ src/App.js
@@ -33,14 +33,24 @@ class App extends Component {
   }
 
   render(){
+
+    const styleButton = {
+      backgroudColor:'white',
+      font: 'inherit',
+      border: '1px solid black',
+      padding:'8px',
+      cursor: 'pointer'
+    }
+
+
     return (
-      <div>
+      <div className="App">
         <h1>Hello there!</h1>
-        <button onClick={() => this.switchJediName("Obi")}>Get Jedi info during the Empire's time!</button>
+        <button style={styleButton} onClick={() => this.switchJediName("Ben Kenobi")}>Get Jedi info during the Empire's time!</button>
         <Jedi 
           name = {this.state.jedies[0].name} 
           age={this.state.jedies[0].age}
-          click = {this.switchJediName.bind(this, "Ben Kenobi")} />
+          click = {this.switchJediName.bind(this, "Obi")} />
         <Jedi 
           name = {this.state.jedies[1].name} 
           age={this.state.jedies[1].age} 
--- src/Jedi/Jedi.js
+++ src/Jedi/Jedi.js
@@ -1,8 +1,9 @@
 import React from "react"
+import './Jedi.css'
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Jedi">
             <p onClick = {props.click}>I'm {props.name}, and spent time as a jedi for {props.age} years.</p>
             <p>{props.children}</p>
             <input type="text" onChange = {props.changed} value={props.name} />
