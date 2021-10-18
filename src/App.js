1caf5c6f742e2902c4509adbe25ffe696d9e987d
Zanghetsu <boythasx@gmail.com>
2021-10-18 16:38:12 +0200

    Added some dynamic content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div>
       <h1>Hello there!</h1>
+      <Person/>
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const person = () => {
+    return <p1>I'm Obi-Wan Kenobi (later known as the non describeble Ben Kenobi), and spent time as a jedi for {Math.floor(Math.random()*30)} years.</p1>
+}
+
+export default person;
