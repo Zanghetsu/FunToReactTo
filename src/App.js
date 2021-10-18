ea8c6f1c741222965e183d58a41eec02a37f3566
Zanghetsu <boythasx@gmail.com>
2021-10-18 17:23:27 +0200

    Implemented some basic state change

--- src/App.js
+++ src/App.js
@@ -8,16 +8,25 @@ class App extends Component {
       {name : "Obi-Wan Kenobi", age : 24},
       {name : "Master Yoda", age : 860},
       {name : "Mace Windu", age : 37}
-      
      ]
   }
 
+  switchJediName = () => {
+    this.setState({
+      jedies: [
+        {name : "Ben Kenobi", age : 42},
+        {name : "Master Yoda", age : 900},
+        {name : "Mace Windu", age : 37}
+       ],
+       otherState: 'some other value'
+    })
+  }
 
   render(){
     return (
       <div>
         <h1>Hello there!</h1>
-        <button>Get Jedi!</button>
+        <button onClick={this.switchJediName}>Get Jedi info during the Empire's time!</button>
         <Jedi name = {this.state.jedies[0].name} age={this.state.jedies[0].age} />
         <Jedi name = {this.state.jedies[1].name} age={this.state.jedies[1].age} />
         <Jedi name = {this.state.jedies[2].name} age={this.state.jedies[2].age} />
