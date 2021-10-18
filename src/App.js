173176de88942aaa3e9c12f0504eb36296da0cac
Zanghetsu <boythasx@gmail.com>
2021-10-18 16:53:16 +0200

    Down graded App() back to class base, to experiment with state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import React,{ Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello there!</h1>
-      <Person name="Obi-Wan Kenobi" age="24" />
-      <Person name="Master Yoda" age="860">Funny this is...</Person>
-      <Person name="Mace Windu" age="37" />
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <h1>Hello there!</h1>
+        <button>Get Jedi!</button>
+        <Person name="Obi-Wan Kenobi" age="24" />
+        <Person name="Master Yoda" age="860">Funny this is...</Person>
+        <Person name="Mace Windu" age="37" />
+      </div>
+    );
+  }
 }
 
 export default App;
