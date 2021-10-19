56d5f381e566acbd805cf005f21f3c7b571a0c91
Zanghetsu <boythasx@gmail.com>
2021-10-19 12:07:16 +0200

    Radium & StyleRoot to work with

--- src/Jedi/Jedi.js
+++ src/Jedi/Jedi.js
@@ -1,13 +1,20 @@
 import React from "react"
 import './Jedi.css'
+import Radium from "radium"
 
-const person = (props) => {
+const jedi = (props) => {
+    const jediStyle = {
+        '@media (min-width: 500px)':{
+            width : '450px'
+        }
+    }
+
     return (
-        <div className="Jedi">
+        <div className="Jedi" style={jediStyle}>
             <p onClick = {props.click}>I'm {props.name}, and spent time as a jedi for {props.age} years.</p>
             <p>{props.children}</p>
             <input type="text" onChange = {props.changed} value={props.name} />
         </div>
     )}
 
-export default person;
+export default Radium(jedi);
