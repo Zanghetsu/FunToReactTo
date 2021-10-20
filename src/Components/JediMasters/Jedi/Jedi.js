91f60f1a0927a0f255f1b38562a0a5a0edd5130e
Zanghetsu <boythasx@gmail.com>
2021-10-20 12:56:03 +0200

    Refactored Jedi component to class based

--- src/Components/JediMasters/Jedi/Jedi.js
+++ src/Components/JediMasters/Jedi/Jedi.js
@@ -1,17 +1,20 @@
-import React from "react"
+import React, {Component} from "react"
 import jediCssStyles from './Jedi.module.css'
 
 
-const jedi = (props) =>{
-    console.log('[Jedi.js] rendering...')
-    return (
-        //<div className="Jedi" style={jediStyle}>
-        <div className={jediCssStyles.Jedi}>
-            <p onClick = {props.click}>I'm {props.name}, and spent time as a jedi for {props.age} years.</p>
-            <p>{props.children}</p>
-            <input type="text" onChange = {props.changed} value={props.name} />
-        </div>
-    )
+class Jedi extends Component {
+    render(){
+        return (
+            //<div className="Jedi" style={jediStyle}>
+            <div className={jediCssStyles.Jedi}>
+                <p onClick = {this.props.click}>I'm {this.props.name}, and spent time as a jedi for {this.props.age} years.</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange = {this.props.changed} value={this.props.name} />
+            </div>
+        )
+    }
+
+    
 }
 
-export default jedi;
+export default Jedi;
